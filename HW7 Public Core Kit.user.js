f0967f6f185f49b1f2ceec1325b3913604da3b02
lvl11evelyn <archenvi@icloud.com>
2026-09-01 21:04:20 -0400

    Bump version from 2.37 to 2.44

--- HW7 Public Core Kit.user.js
+++ HW7 Public Core Kit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HW7 Core Kit
 // @namespace    hw-7-tracking-panel-kit
-// @version      2.37
+// @version      2.44
 // @description  Unified public access build for HoboWars
 // @author       lvl11evelyn / sɛvɜn (2924238)
 // @license      All Rights Reserved
